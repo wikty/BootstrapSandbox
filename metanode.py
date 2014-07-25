93355008afaaf9ceca7310599e52d7177b0ec424
wikty <wiktymouse@gmail.com>
2014-07-25 21:49:53 +0800

    update

--- metanode.py
+++ metanode.py
@@ -15,23 +15,19 @@ class ContentMetaNode(object):
 
 @functools.total_ordering
 class BlockMetaNode(object):
-    def __init__(self, name, start, end, content_start, content_end):
+    def __init__(self, block, start, end, content_start, content_end):
         if (start>end) or (content_start>content_end):
             raise Exception('Block start should litter than end')
-        self.name = name
+        self.name = block
         self.start = start
         self.end = end
         self.content_start = content_start
         self.content_end = content_end
-        self.block_list = []
-        self.content_list = []
+        self.item_list = []
     
     def __gt__(self, other):
-        return (self.start > other.end) and (self.end > other.end)
-    
-    def __lt__(self, other):
-        return (self.end < other.start) and (self.start < other.start)
-    
+        return self.start > other.start
+
     def __eq__(self, other):
         return (self.start == other.start) and (self.end == other.end)
     
@@ -41,25 +37,47 @@ class BlockMetaNode(object):
         else:
             raise Exception('Only BlockMetaNode and ContentMetaNode Can Operator In With BlockMetaNode')
     
+    def issiblings(self, other):
+        if self.end < other.start:
+            # previous siblings
+            return 1
+        elif self.start > other.end:
+            # next siblings
+            return 2
+    
+    def add_item(self, block_or_content):
+        if isinstance(block_or_content, (BlockMetaNode, ContentMetaNode)):
+            self.item_list.append(block_or_content)
+        else:
+            raise Exception('Must Add BlockMetaNode or ContentMetaNode object')
+
+class FileMetaNode(object):
+    def __init__(self, name, root=False):
+        self.name = name
+        self.item_list = []
+        self.child = None
+        self.root = root
+
     def add_block(self, block):
         if isinstance(block, BlockMetaNode):
-            self.block_list.append(block)
+            self.item_list.append(block)
         else:
             raise Exception('Must Add BlockMetaNode object')
     
     def add_content(self, content):
-        if isinstance(content, ContentMetaNode):
-            self.content_list.append(content)
+        if not self.root:
+            raise Exception('Only Root File can Add ContentMetaNode')
         else:
-            raise Exception('Must Add ContentMetaNode object')
-
-class FileMetaNode(object):
-    def __init__(self, name):
-        self.name = name
-        self.block_list = []
-
-    def add_block(self, block):
-        if isinstance(block, BlockMetaNode):
-            self.block_list.append(block)
+            if not isinstance(content, ContentMetaNode):
+                raise Exception('Must Add ContentMetaNode object')
+            else:
+                self.item_list.append(content)
+    
+    def get_all_blocks(self):
+        return [block for block in self.item_list if isinstance(block, BlockMetaNode)]
+    
+    def set_child(self, filenode):
+        if isinstance(filenode, FileMetaNode):
+            self.child = filenode
         else:
-            raise Exception('Must Add BlockMetaNode object')
+            raise Exception('Must Set a FileMetaNode as child')
