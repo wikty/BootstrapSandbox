0d4d8bc5f0688bbb0d9488d57e2f49cc4f7e23c8
wikty <wiktymouse@gmail.com>
2014-07-25 21:50:39 +0800

    update

--- testbox.py
+++ testbox.py
@@ -3,7 +3,7 @@ import sys
 import string
 
 from config import *
-from utils import get_extend_tree, find_all_blocks
+from utils import get_extend_dict, find_all_blocks
 
 
 def help():
@@ -18,7 +18,7 @@ def help():
     })
     
     arg = sys.argv[1]
-    if not os.path.isfile(arg):
+    if not os.path.isfile(os.path.join(WORKING_DIR, arg)):
         return helptext
 
 def main():
@@ -32,16 +32,19 @@ def main():
         base_blocks = find_all_blocks(base_content)
     
     for input_file in sys.argv[1:]:
+        if not input_file.startswith(WORKING_DIR):
+            input_file = os.path.join(WORKING_DIR, input_file)
         if not os.path.isfile(input_file):
             print('')
             print('------> File *%s* does not exist!!!' % input_file)
         else:
             generated_file = os.path.join(os.path.dirname(input_file), os.path.basename(input_file)+'.gen.html')
             print('')
-            print('------> File *%s* will be generated from %s ...' % (generated_file, input_file))
+            print('------> File *%s* will be generated from *%s*...' % (generated_file, input_file))
         
-        extend_tree = get_extend_tree(input_file, BASE_FILE)
-        print(extend_tree)
+        extend_tree = get_extend_dict(input_file, BASE_FILE)
+        extend_tree.popitem()
+        print(extend_tree.popitem())
         
         
 
