da4417a4f0cef2d9febe64d7b377fcb567a42fb0
wikty <wiktymouse@gmail.com>
2014-07-27 11:16:29 +0800

    add comment

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
 
+# You should change WORKING_DIR to your workspace directory path
 WORKING_DIR = 'examples'
+# Default using the WORKING_DIR/base.html as the base template file
+# You may want to specify yourself base template file
 BASE_FILE = os.path.join(os.path.dirname(__file__), WORKING_DIR, 'base.html')
