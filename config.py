e872b5b3657804e23943fa12dc31d65afaabc85c
wikty <wiktymouse@gmail.com>
2014-07-27 14:10:28 +0800

    add server variable

--- config.py
+++ config.py
@@ -5,3 +5,9 @@ WORKING_DIR = 'examples'
 # Default using the WORKING_DIR/base.html as the base template file
 # You may want to specify yourself base template file
 BASE_FILE = os.path.join(os.path.dirname(__file__), WORKING_DIR, 'base.html')
+# Genereted file suffix name
+SUFFIX_NAME = '.gen.html'
+# Server hostname
+HOSTNAME = 'localhost'
+# Server Listen on port
+PORT = 5000
